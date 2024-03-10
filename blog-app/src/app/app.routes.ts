efb3a4a5f0f6f05e3ba5fcb1c974470612f81954
Marcel <marcelpodsiadlo.business@gmail.com>
2024-03-10 15:58:18 +0100

    I added new components (policy and social media), add new style to other files

--- blog-app/src/app/app.routes.ts
+++ blog-app/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './account/login/login.component';
 import { SignupComponent } from './account/signup/signup.component';
+import { SocialmediaComponent } from './socialmedia/socialmedia.component';
+import { PolicyComponent } from './policy/policy.component';
+
 
 export const routes: Routes = [
   {path: 'account/login', component: LoginComponent},
-  {path: 'account/signup', component: SignupComponent}
+  {path: 'account/signup', component: SignupComponent},
+  {path: 'socialmedia', component: SocialmediaComponent},
+  {path: 'policy', component: PolicyComponent}
 ];
---
+++ blog-app/src/app/socialmedia/socialmedia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-socialmedia',
+  standalone: true,
+  imports: [],
+  templateUrl: './socialmedia.component.html',
+  styleUrl: './socialmedia.component.scss'
+})
+export class SocialmediaComponent {
+
+}
