5ab60c95dfa40bac38740d38ee4a16cce88a8966
Marcel <marcelpodsiadlo.business@gmail.com>
2024-03-13 15:35:46 +0100

    I created a new folder - components, for all components for better organization, I changed styles and add content to SocialMediaComponent and PolicyComponents

--- blog-app/src/app/app.routes.ts
+++ blog-app/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './account/login/login.component';
-import { SignupComponent } from './account/signup/signup.component';
-import { SocialmediaComponent } from './socialmedia/socialmedia.component';
-import { PolicyComponent } from './policy/policy.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { SignupComponent } from './components/account/signup/signup.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { PolicyComponent } from './components/policy/policy.component';
 
 
 export const routes: Routes = [
