7f10bd3468e1b53e18470486dccaa69f10861bbb
Marcel <marcelpodsiadlo.business@gmail.com>
2024-03-28 15:53:08 +0100

    I added a forms control and validation to signup for. I created an error message with *ngIf for users

--- blog-app/src/app/components/account/account.service.ts
+++ blog-app/src/app/components/account/account.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, Validators  } from '@angular/forms';
+import { AbstractControl, FormBuilder, Validators  } from '@angular/forms';
 import { Router} from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
@@ -8,25 +8,52 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class AccountService {
 
+  validateEmail(control: AbstractControl) {
+    const validationEmailWords = ['/', ',', '!', '$', '%', '^', '&', '+', '=', '(', ')', '|', '{', '}', '[', ']', "'", '"', '<', '>', '?']
+    if(control.value && validationEmailWords.some(word => control.value.includes(word))){
+      return {invalidEmail: true}
+    }
+    return null
+  }
+  validatePassword(control: AbstractControl) {
+    const validationPasswordWords = ['/', ',', '!', '$', '%', '^', '&', '+', '=', '(', ')', '|', '{', '}', '[', ']', "'", '"', '<', '>', '?', '@']
+    if (control.value && control.value.length < 8) {
+      return {noLetters: true}
+    }
+    if (control.value && validationPasswordWords.some(word => control.value.includes(word))) {
+      return {invalidPassword: true}
+    }
+    return null
+  }
+
+  submitted = false
   signupForm = this.fb.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
+    email: ['', [Validators.required, this.validateEmail]],
+    password: ['', [Validators.required, this.validatePassword]],
   })
   constructor(private fb: FormBuilder, private afAuth: AngularFireAuth, private router: Router) { }
 
   async onSubmit() {
-    const email = this.signupForm.value.email
-    const password = this.signupForm.value.password
-    if (email && password) {
-      try {
-        await this.afAuth.createUserWithEmailAndPassword(email, password);
-        this.router.navigate(['/'])
-        alert('Successfully signed up!')
-      } catch (error) {
-        console.log
+    this.submitted = true
+    if (this.signupForm.valid) {
+      const email = this.signupForm.value.email
+      const password = this.signupForm.value.password
+
+      if(email && password){
+        try {
+          await this.afAuth.createUserWithEmailAndPassword(email, password)
+          this.router.navigate(['/'])
+          console.log("User created")
+        } catch (error) {
+          this.router.navigate(['/signup'])
+          console.log(error)
+        }
+      } else {
+
       }
-    } else {
-      // Handle missing email or password here
+    }
+    else if(this.signupForm.invalid){
+
     }
   }
 }
--- blog-app/src/app/components/account/signup/signup.component.ts
+++ blog-app/src/app/components/account/signup/signup.component.ts
@@ -8,11 +8,24 @@ import { FormGroup} from '@angular/forms';
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-  constructor(private accountService: AccountService){ }
+  constructor(private accountService: AccountService){}
 
   get signupForm(): FormGroup {
     return this.accountService.signupForm;
   }
+  // Email and Passoword
+  // Here we are using getters to access the email and password form controls
+  get submitted() {
+    return this.accountService.submitted
+  }
+  get email() {
+    return this.signupForm.get('email');
+  }
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+
 
   onSubmit() {
     this.accountService.onSubmit()
