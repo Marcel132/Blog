66746e896936342122ae50a513c9450f90ad674e
Marcel Podsiadło <marcelpodsiadlo.business@gmail.com>
2024-06-08 11:00:10 +0200

    Update environments.ts

--- blog-app/src/app/environments/environments.ts
+++ blog-app/src/app/environments/environments.ts
@@ -1,13 +1,3 @@
 //! Firebase configuration
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyC9JOF51DQIdKgJusxjf57K29yn7z5myUc",
-    authDomain: "blog-app-b711b.firebaseapp.com",
-    databaseURL: "https://blog-app-b711b-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "blog-app-b711b",
-    storageBucket: "blog-app-b711b.appspot.com",
-    messagingSenderId: "732180813490",
-    appId: "1:732180813490:web:371f7916073eb49e195bab"
-  }
 }
